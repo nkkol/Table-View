ce678319dad2f40c47e5324f82f3a94e88e807eb
Inna <litvinenko.odindvatri@gmail.com>
2020-03-21 00:18:27 +0200

    Initial Commit

---
+++ Table View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Table View
+//
+//  Created by Inna Litvinenko on 3/21/20.
+//  Copyright © 2020 Inna Litvinenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
